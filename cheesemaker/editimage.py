a3b6ebc78abf4c091d87305e402b095c984a6c8b
David Whitlock <alovedalongthe@gmail.com>
2014-02-10 19:47:50 +0700

    Simplified edit image

--- cheesemaker/editimage.py
+++ cheesemaker/editimage.py
@@ -17,21 +17,30 @@
 # You should have received a copy of the GNU General Public License
 # along with Cheesemaker.  If not, see <http://www.gnu.org/licenses/gpl.html>.
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QGridLayout, QLabel, QSpinBox, QCheckBox
 
-class ResizeDialog(QtWidgets.QDialog):
+class SpinBox(QSpinBox):
+    def __init__(self, val, maxval, step, func):
+        QSpinBox.__init__(self)
+
+        self.setRange(0, maxval)
+        self.setValue(val)
+        self.setSingleStep(step)
+        self.valueChanged.connect(func)
+
+class ResizeDialog(QDialog):
     def __init__(self, parent, width, height):
-        QtWidgets.QDialog.__init__(self, parent)
+        QDialog.__init__(self, parent)
 
         self.setWindowTitle('Resize image')
         self.ratio = width / height
-        layout = QtWidgets.QGridLayout()
+        layout = QGridLayout()
         self.setLayout(layout)
 
         self.set_resize_view(layout, width, height)
         self.set_aspratio_view(layout)
 
-        buttons = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel)
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
         layout.addWidget(buttons)
@@ -40,25 +49,17 @@ class ResizeDialog(QtWidgets.QDialog):
         self.show()
 
     def set_resize_view(self, layout, width, height):
-        layout.addWidget(QtWidgets.QLabel('Width'), 0, 0, 1, 1)
-        self.get_width = QtWidgets.QSpinBox()
-        self.get_width.setRange(0, width)
-        self.get_width.setValue(width)
-        self.get_width.setSingleStep(10)
-        self.get_width.valueChanged.connect(self.width_changed)
+        layout.addWidget(QLabel('Width'), 0, 0, 1, 1)
+        self.get_width = SpinBox(width, width, 10, self.width_changed)
         layout.addWidget(self.get_width, 0, 1, 1, 1)
 
-        layout.addWidget(QtWidgets.QLabel('Height'), 1, 0, 1, 1)
-        self.get_height = QtWidgets.QSpinBox()
-        self.get_height.setRange(0, height)
-        self.get_height.setValue(height)
-        self.get_height.setSingleStep(10)
-        self.get_height.valueChanged.connect(self.height_changed)
+        layout.addWidget(QLabel('Height'), 1, 0, 1, 1)
+        self.get_height = SpinBox(height, height, 10, self.height_changed)
         layout.addWidget(self.get_height, 1, 1, 1, 1)
 
     def set_aspratio_view(self, layout):
         self.pres_aspratio = True
-        self.aspratio = QtWidgets.QCheckBox('Preserve aspect ratio')
+        self.aspratio = QCheckBox('Preserve aspect ratio')
         self.aspratio.setChecked(True)
         self.aspratio.toggled.connect(self.toggle_aspratio)
         layout.addWidget(self.aspratio, 2, 0, 1, 2)
@@ -80,20 +81,20 @@ class ResizeDialog(QtWidgets.QDialog):
     def toggle_aspratio(self):
         self.pres_aspratio = self.aspratio.isChecked()
 
-class CropDialog(QtWidgets.QDialog):
+class CropDialog(QDialog):
     def __init__(self, parent, width, height):
-        QtWidgets.QDialog.__init__(self, parent)
+        QDialog.__init__(self, parent)
 
         self.setWindowTitle('Crop image')
         self.draw = parent.img_view.crop_draw
         self.new_width = self.width = width
         self.new_height = self.height = height
 
-        layout = QtWidgets.QGridLayout()
+        layout = QGridLayout()
         self.setLayout(layout)
         self.set_crop_view(layout)
 
-        buttons = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel)
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
         layout.addWidget(buttons)
@@ -102,36 +103,20 @@ class CropDialog(QtWidgets.QDialog):
         self.show()
 
     def set_crop_view(self, layout):
-        layout.addWidget(QtWidgets.QLabel('Distance from the left'), 0, 0, 1, 1)
-        self.get_lx = QtWidgets.QSpinBox()
-        self.get_lx.setRange(0, self.width - 1)
-        self.get_lx.setValue(0)
-        self.get_lx.setSingleStep(5)
-        self.get_lx.valueChanged.connect(self.lx_changed)
+        layout.addWidget(QLabel('Distance from the left'), 0, 0, 1, 1)
+        self.get_lx = SpinBox(0, self.width - 1, 5, self.lx_changed)
         layout.addWidget(self.get_lx, 0, 1, 1, 1)
 
-        layout.addWidget(QtWidgets.QLabel('Distance from the right'), 1, 0, 1, 1)
-        self.get_rx = QtWidgets.QSpinBox()
-        self.get_rx.setRange(0, self.width - 1)
-        self.get_rx.setValue(0)
-        self.get_rx.setSingleStep(5)
-        self.get_rx.valueChanged.connect(self.rx_changed)
+        layout.addWidget(QLabel('Distance from the right'), 1, 0, 1, 1)
+        self.get_rx = SpinBox(0, self.width - 1, 5, self.rx_changed)
         layout.addWidget(self.get_rx, 1, 1, 1, 1)
 
-        layout.addWidget(QtWidgets.QLabel('Distance from the top'), 2, 0, 1, 1)
-        self.get_ty = QtWidgets.QSpinBox()
-        self.get_ty.setRange(0, self.height - 1)
-        self.get_ty.setValue(0)
-        self.get_ty.setSingleStep(5)
-        self.get_ty.valueChanged.connect(self.ty_changed)
+        layout.addWidget(QLabel('Distance from the top'), 2, 0, 1, 1)
+        self.get_ty = SpinBox(0, self.height - 1, 5, self.ty_changed)
         layout.addWidget(self.get_ty, 2, 1, 1, 1)
 
-        layout.addWidget(QtWidgets.QLabel('Distance from the bottom'), 3, 0, 1, 1)
-        self.get_by = QtWidgets.QSpinBox()
-        self.get_by.setRange(0, self.height - 1)
-        self.get_by.setValue(0)
-        self.get_by.setSingleStep(5)
-        self.get_by.valueChanged.connect(self.by_changed)
+        layout.addWidget(QLabel('Distance from the bottom'), 3, 0, 1, 1)
+        self.get_by = SpinBox(0, self.height - 1, 5, self.by_changed)
         layout.addWidget(self.get_by, 3, 1, 1, 1)
 
     def lx_changed(self):
